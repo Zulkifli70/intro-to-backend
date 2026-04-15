b67ead659e392247269c0a49ac183ca8d0af811c
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2026-04-15 10:29:49 +0700

    1

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,1 +1,15 @@
 import mongoose from "mongoose";
+
+const connection = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}`,
+    );
+    console.log(`\n Connected ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+export default connection;
