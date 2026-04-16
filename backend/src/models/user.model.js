7ae0b968bf4d33273a40965fae76e73e431aa810
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2026-04-16 12:52:25 +0700

    make user table

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      minLength: 1,
+      maxlength: 30,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = mongoose.model("User", userSchema);
